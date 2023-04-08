7147233236d165426f98f9ac6b6b91690e132bb0
Awele1111 <awelelan@Aweles-MacBook-Air.local>
2023-04-08 03:13:32 -0500

    adding js folder files

---
+++ public/js/comment.js
@@ -0,0 +1,28 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector("textarea[name='comment-body']").value;
+
+    // extract post id
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1
+    ];
+
+    // users cannot submit an empty comment
+    if (comment_text) {
+        const response = await fetch(`/api/comments`, {
+            method: "POST", 
+            body: JSON.stringify({ post_id, comment_text }),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        };
+    };
+};
+
+// event listener
+document.querySelector(".comment-form").addEventListener("submit", commentFormHandler);
